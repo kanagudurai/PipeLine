0949a40a7f74745438fe064f1c2a2614ff569776
KanaguduraiB <kanagudurai.b@photoninfotech.net>
2021-11-05 14:30:54 +0530

    Done

--- iOSHelloWorldTests/iOSHelloWorldTests.swift
+++ iOSHelloWorldTests/iOSHelloWorldTests.swift
@@ -9,7 +9,7 @@
 @testable import iOSHelloWorld
 import XCTest
 
-class iOSHelloWorldTests: XCTestCase {
+class IOSHelloWorldTests: XCTestCase {
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
--- iOSHelloWorldUITests/iOSHelloWorldUITests.swift
+++ iOSHelloWorldUITests/iOSHelloWorldUITests.swift
@@ -8,7 +8,7 @@
 
 import XCTest
 
-class iOSHelloWorldUITests: XCTestCase {
+class IOSHelloWorldUITests: XCTestCase {
     override func setUp() {
         super.setUp()
 
