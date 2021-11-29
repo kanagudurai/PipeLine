f3c55ea4947d659ba7df5d540f71dcb967a1a4d0
KanaguduraiB <kanagudurai.b@photoninfotech.net>
2021-11-29 15:36:49 +0530

    updated

---
+++ iOSHelloWorldTests/ScanningControllerTests.swift
@@ -0,0 +1,26 @@
+//
+//  ScanningControllerTests.swift
+//  DevSetupTests
+//
+//  Created by Kanagudurai on 15/11/21.
+//
+
+import XCTest
+import StandardCyborgFusion
+import StandardCyborgUI
+
+@testable import iOSHelloWorld
+
+class ScanningControllerTests: XCTestCase {
+
+    
+    func testInitialView() {
+        ScanningHapticFeedbackEngine.shared.scanningFinished()
+    }
+    
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
